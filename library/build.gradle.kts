74e35d03505d673ad29f24a86f101d092155b52d
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2025-05-12 22:43:03 +0000

    chore: remove versionCode and versionName from defaultConfig in build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -7,8 +7,6 @@ android {
 
     defaultConfig {
         minSdk = 35
-        versionCode = 8
-        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
