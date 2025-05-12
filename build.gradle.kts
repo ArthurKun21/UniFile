b3061522b9809d5ec9695714258c98b43c653f99
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2025-05-12 22:24:54 +0000

    chore: refactor build.gradle.kts to use Kotlin DSL and simplify clean task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-buildscript {
-    repositories {
-        mavenCentral()
-        google()
-    }
-    dependencies {
-        classpath 'com.android.tools.build:gradle:4.1.3'
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
 }
 
-allprojects {
-    repositories {
-        mavenCentral()
-        google()
-    }
-}
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean") {
+    group = "build"
+    description = "Deletes build directory"
+
+    delete(rootProject.layout.buildDirectory)
 }
