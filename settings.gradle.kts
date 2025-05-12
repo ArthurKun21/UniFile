7018c8c60784c6601c41adca3417d6a4aeb8e5a8
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2025-05-12 22:25:49 +0000

    chore: restructure settings.gradle.kts to enhance plugin and dependency management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,33 @@
-include ':app', ':library'
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        // fallback for the rest of the dependencies
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+
+    }
+}
+
+include(":app")
+include(":library")
